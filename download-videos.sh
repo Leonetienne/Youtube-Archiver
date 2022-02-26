5f61808f55932268bbf618a6c5eae42b995534ca
Leonetienne <leonetienne@hotmail.de>
2022-02-26 14:24:49 +0100

    Removed legacy scripts

--- download-videos.sh
+++
@@ -1,9 +0,0 @@
-yt-dlp \
--a linklist-videos.txt \
--c \
---merge-output-format mkv \
--o 'videos/%(uploader)s/%(title)s.%(ext)s' \
---extractor-args youtube:player_client=android \
---throttled-rate 100M \
---restrict-filenames \
---download-archive alreadydownloaded-videos.txt
