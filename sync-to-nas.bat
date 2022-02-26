209b5ad526b2429be2b01d0efa9537f7bb3a8705
Leonetienne <leonetienne@hotmail.de>
2022-02-26 14:54:58 +0100

    Added nice comment

--- sync-to-nas.bat
+++ sync-to-nas.bat
@@ -1,3 +1,6 @@
+Rem Since i use WSL, and like to store my archives on my nas, i made a quick script to move downloads from my local instance to my nas.
+Rem These are the important files to move, btw.
+
 robocopy music\ Y:\Leon\Mediafarm\Youtube-Archive\music\ /e /z /j /xo /move
 robocopy videos\ Y:\Leon\Mediafarm\Youtube-Archive\videos\ /e /z /j /xo /move
 robocopy alreadydownloaded*.txt Y:\Leon\Mediafarm\Youtube-Archive\ /z
