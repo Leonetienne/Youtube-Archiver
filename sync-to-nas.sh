76fdc8f43bb3a2c79485fd6e0806cf15930bd307
Leonetienne <leon@etiennes.de>
2022-03-23 20:41:24 +0100

    Fix nas script

--- sync-to-nas.sh
+++ sync-to-nas.sh
@@ -1,12 +1,8 @@
 #!/bin/zsh
 
-REMOTE_LOCATION = /media/family_nas/Leon/Mediafarm/Youtube-Archive
+REMOTE_LOCATION=/media/family_nas/Leon/Mediafarm/Youtube-Archive
 
-mv music/ $REMOTE_LOCATION
-mv videos/ $REMOTE_LOCATION
-mv alreadydownloaded*.txt $REMOTE_LOCATION
-
-# robocopy music\ Y:\Leon\Mediafarm\Youtube-Archive\music\ /e /z /j /xo /move
-# robocopy videos\ Y:\Leon\Mediafarm\Youtube-Archive\videos\ /e /z /j /xo /move
-# robocopy . Y:\Leon\Mediafarm\Youtube-Archive\ alreadydownloaded*.txt /z /im
+cp -u alreadydownloaded.txt $REMOTE_LOCATION
+rsync -auv --remove-source-files music/ "$REMOTE_LOCATION"/music
+rsync -auv --remove-source-files videos/ "$REMOTE_LOCATION"/videos
 
